ef346f57b4c2537a5b03b5c52d057fbd7ba37d55
megha rana <megharanait95@gmail.com>
2019-02-19 23:39:18 +0530

    parsing csv done

--- src/ClubOffice/loanpart/management/commands/retrieve.py
+++ src/ClubOffice/loanpart/management/commands/retrieve.py
@@ -1,21 +1,51 @@
 from django.core.management.base import BaseCommand, CommandError
+from loanpart.models import LoanPart
+import csv
+from decimal import Decimal
 
 
 class Command(BaseCommand):
     help = 'Closes the specified poll for voting'
 
     def add_arguments(self, parser):
-
         pass
 
     def handle(self, *args, **options):
-        # for poll_id in options['poll_id']:
-        #     try:
-        #         poll = Poll.objects.get(pk=poll_id)
-        #     except Poll.DoesNotExist:
-        #         raise CommandError('Poll "%s" does not exist' % poll_id)
+        with open('/Users/megharana/Downloads/loan_part_0009b606f.csv'
+                  ) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            count_own = 0
+            for row in csv_reader:
+                if count_own > 0:
+                    entry = LoanPart(
+                        mem_id=row[0],
+                        loan_amnt=int(row[1]),
+                        funded_amnt_inv=int(row[2]),
+                        term=row[3],
+                        int_rate=Decimal(row[4]),
+                        installment=row[5],
+                        grade=row[6],
+                        emp_title=row[7],
+                        emp_length=row[8],
+                        home_ownership=row[9],
+                        annual_inc=row[10],
+                        verification_status=row[11],
+                        issue_d=row[12],
+                        loan_status=row[13],
+                        desc=row[14],
+                        purpose=row[15],
+                        title=row[16],
+                        addr_state=row[17],
+                        last_pymnt_d=row[18],
+                        last_pymnt_amnt=row[19])
+                    entry.save()
+                count_own += 1
+            print(count_own)
 
-        #     poll.opened = False
-        #     poll.save()
 
-        self.stdout.write("Commands.retrive is accessible")
+# import pandas
+# import copy
+# df = pandas.read_csv(
+#     '/Users/megharana/Downloads/loan_part_0009b606f.csv', delimiter=",")
+
+# print(df[df['home_ownership'].str.contains('RENT')])
--- src/ClubOffice/loanpart/models.py
+++ src/ClubOffice/loanpart/models.py
@@ -7,13 +7,15 @@ class LoanPart(models.Model):
     loan_amnt = models.IntegerField()
     funded_amnt_inv = models.IntegerField()
     term = models.CharField(max_length=20)
-    int_rate = models.IntegerField()
-    installment = models.IntegerField()
+    int_rate = models.DecimalField(max_digits=50, decimal_places=5, default="")
+    installment = models.DecimalField(
+        max_digits=50, decimal_places=3, default="")
     grade = models.CharField(max_length=1)
     emp_title = models.CharField(max_length=100, null=True, blank=True)
     emp_length = models.CharField(max_length=120)
     home_ownership = models.CharField(max_length=30)
-    annual_inc = models.IntegerField()
+    annual_inc = models.models.DecimalField(
+        max_digits=50, decimal_places=3, default="")
     verification_status = models.CharField(max_length=20)
     issue_d = models.CharField(max_length=20)
     loan_status = models.CharField(max_length=20)
@@ -21,8 +23,9 @@ class LoanPart(models.Model):
     purpose = models.CharField(max_length=20)
     title = models.CharField(max_length=20)
     addr_state = models.CharField(max_length=20)
-    last_pymnt_d = models.CharField(max_length=20)
-    last_pymnt_amnt = models.CharField(max_length=20)
+    last_pymnt_d = models.CharField(max_length=20, null=True, blank=True)
+    last_pymnt_amnt = models..DecimalField(
+        max_digits=50, decimal_places=3, default="")
 
 
 # class LoanPart_MORTGAGE(models.Model):
